95f29a37bc9e3d79b19f34118fa89abf93c6ab5e
TigranMagh <tigran.maghaqyan6@gmail.com>
2024-11-07 19:12:24 +0400

    onStrat onDestory onPaus

--- app/src/main/java/com/example/myfragmentapplication/MainActivity.java
+++ app/src/main/java/com/example/myfragmentapplication/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.myfragmentapplication;
 
+import static androidx.core.content.ContentProviderCompat.requireContext;
+
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
@@ -16,24 +19,18 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int bottomPosition;
+    private static final String PREFS_NAME = "MyPreferences";
+    private static final String KEY_BOTTOM_BAR_POSITION = "bottom_bar_position";
+    SharedPreferences settings;
+    SharedPreferences.Editor prefEditor;
+
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
-        bottomPosition = sharedPreferences.getInt("bottomPosition", 0);
-        if (bottomPosition == 0) {
-            replaceFragment(new HomeFragment());
-        } else if (bottomPosition == 1) {
-            replaceFragment(new SearchFragment());
-        } else if (bottomPosition == 2) {
-            replaceFragment(new LoginFragment());
-        } else if (bottomPosition == 3) {
-            replaceFragment(new WebFragment());
-        } else if (bottomPosition == 4) {
-            replaceFragment(new ExitFragment());
-        }
+    protected void onStart() {
+        super.onStart();
+        prefEditor = settings.edit();
+        prefEditor.putInt(KEY_BOTTOM_BAR_POSITION, settings.getInt(KEY_BOTTOM_BAR_POSITION,0));
+        prefEditor.apply();
     }
 
     @Override
@@ -48,24 +45,55 @@ public class MainActivity extends AppCompatActivity {
         });
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        //replaceFragment(new HomeFragment());
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        settings = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        int position = settings.getInt(KEY_BOTTOM_BAR_POSITION,0);
+
+        if (position==0){
+            replaceFragment(new HomeFragment());
+            bottomNavigationView.setSelectedItemId(R.id.nav_home);
+        } else if (position==1) {
+            replaceFragment(new SearchFragment());
+            bottomNavigationView.setSelectedItemId(R.id.nav_search);
+        }else if (position==2){
+            bottomNavigationView.setSelectedItemId(R.id.nav_login);
+            replaceFragment(new LoginFragment());
+        } else if (position==3) {
+            bottomNavigationView.setSelectedItemId(R.id.nav_web);
+            replaceFragment(new WebFragment());
+        } else if (position==4) {
+            bottomNavigationView.setSelectedItemId(R.id.nav_exit);
+            replaceFragment(new ExitFragment());
+        }
+
+
         bottomNavigationView.setOnItemSelectedListener(item -> {
 
             if (item.getItemId() == R.id.nav_home) {
-                bottomPosition = 0;
+                prefEditor = settings.edit();
+                prefEditor.putInt(KEY_BOTTOM_BAR_POSITION, 0);
+                prefEditor.apply();
                 replaceFragment(new HomeFragment());
             } else if (item.getItemId() == R.id.nav_search) {
-                bottomPosition = 1;
+                prefEditor = settings.edit();
+                prefEditor.putInt(KEY_BOTTOM_BAR_POSITION, 1);
+                prefEditor.apply();
                 replaceFragment(new SearchFragment());
             } else if (item.getItemId() == R.id.nav_login) {
-                bottomPosition = 2;
+                prefEditor = settings.edit();
+                prefEditor.putInt(KEY_BOTTOM_BAR_POSITION, 2);
+                prefEditor.apply();
                 replaceFragment(new LoginFragment());
             } else if (item.getItemId() == R.id.nav_web) {
-                bottomPosition = 3;
+                prefEditor = settings.edit();
+                prefEditor.putInt(KEY_BOTTOM_BAR_POSITION, 3);
+                prefEditor.apply();
                 replaceFragment(new WebFragment());
             } else if (item.getItemId() == R.id.nav_exit) {
-                bottomPosition = 4;
+                prefEditor = settings.edit();
+                prefEditor.putInt(KEY_BOTTOM_BAR_POSITION, 4);
+                prefEditor.apply();
                 replaceFragment(new ExitFragment());
             }
             return true;
@@ -81,11 +109,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStop() {
+        super.onStop();
+        prefEditor = settings.edit();
+        prefEditor.putInt(KEY_BOTTOM_BAR_POSITION, settings.getInt(KEY_BOTTOM_BAR_POSITION,0));
+        prefEditor.apply();
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
-        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
-        SharedPreferences.Editor myEdit = sharedPreferences.edit();
-        myEdit.putInt("bottomPosition", bottomPosition);
-        myEdit.apply();
+        prefEditor = settings.edit();
+        prefEditor.putInt(KEY_BOTTOM_BAR_POSITION, settings.getInt(KEY_BOTTOM_BAR_POSITION,0));
+        prefEditor.apply();
     }
 }
